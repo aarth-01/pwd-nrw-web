b1d716c25792b10ed3cdea8be424ea3c2eee42b8
aarth-01 <aarthvajandar01@gmail.com>
2026-04-05 13:19:02 +0530

    Production Web App ready!

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmStDxEzrbFExN1yWk2cdJqxNYvyKJDX4",
-  authDomain: "nrw-realloss.firebaseapp.com",
-  projectId: "nrw-realloss",
-  storageBucket: "nrw-realloss.firebasestorage.app",
-  messagingSenderId: "343904658215",
-  appId: "1:343904658215:web:2d9e54d320e450c696ff02",
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
